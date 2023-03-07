74e51ea179f8dc9dd9e533dfa5a481e6d2a2b9e9
Dawid Wróbel <wrobeldawid1997@gmail.com>
2023-03-07 07:38:15 +0100

    Fixed largestNumberDifference method

--- lib/src/features/home_page/domain/models/chess_game_model.dart
+++ lib/src/features/home_page/domain/models/chess_game_model.dart
@@ -28,33 +28,38 @@ class ChessGameModel {
   Map<String, dynamic> get largestNumberDifference {
     int biggestDifference = 0;
     int moveNumberOfBiggestDifference = 0;
-    for (var i = 0; i < movesAnalysis.length - 1; i++) {
+    for (var i = 1; i < movesAnalysis.length - 1; i++) {
       // If there is a possible mate sequence then no eval is given, so it's impossible to compare, we have to ignore instead
       if (movesAnalysis[i].containsKey('mate') ||
-          movesAnalysis[i + 1].containsKey('mate')) {
+          movesAnalysis[i - 1].containsKey('mate')) {
+        continue;
+      }
+      // If analysis dedects a mistake it always sends more than 1 value and gives best possible move
+      // without analsys we don't know what the best move is so we only want moves with analysis
+      if (movesAnalysis[i].length == 1) {
         continue;
       }
       // We only want to see our mistakes so we ignore moves made by our opponent
       // All moves by white are even and all moves by black are odd so this filters the moves based on which color the user is
       if (userId == whitePlayer) {
-        if ((i % 2) != 0) {
-          continue;
-        }
-      }
-      if (userId == blackPlayer) {
         if ((i % 2) == 0) {
           continue;
         }
       }
+      if (userId == blackPlayer) {
+        if ((i % 2) != 0) {
+          continue;
+        }
+      }
       int currentDifference =
-          movesAnalysis[i]['eval'] - movesAnalysis[i + 1]['eval'];
+          movesAnalysis[i]['eval'] - movesAnalysis[i - 1]['eval'];
       if ((currentDifference).abs() > biggestDifference) {
-        if (userId == whitePlayer && currentDifference > 0) {
+        if (userId == whitePlayer && currentDifference < 0) {
           biggestDifference = (currentDifference).abs();
           moveNumberOfBiggestDifference = i;
-        } else if (userId == blackPlayer && currentDifference < 0) {
+        } else if (userId == blackPlayer && currentDifference > 0) {
           biggestDifference = (currentDifference).abs();
-          moveNumberOfBiggestDifference = i;
+          moveNumberOfBiggestDifference = i - 1;
         }
       }
     }
