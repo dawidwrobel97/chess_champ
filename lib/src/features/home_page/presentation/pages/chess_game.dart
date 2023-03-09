a38928317d12b3d36301ff8d7333ba0f15916467
Dawid Wróbel <wrobeldawid1997@gmail.com>
2023-03-09 08:17:27 +0100

    Added bottom sqares annotation

--- lib/src/features/home_page/presentation/pages/chess_game.dart
+++ lib/src/features/home_page/presentation/pages/chess_game.dart
@@ -4,7 +4,7 @@ import 'package:chess_app/src/features/home_page/presentation/cubits/chess_game_
 import 'package:chess_app/src/features/home_page/domain/models/chess_game_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_chess_board/flutter_chess_board.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart' as chess;
 
 class ChessGame extends StatefulWidget {
   const ChessGame({
@@ -19,6 +19,8 @@ class ChessGame extends StatefulWidget {
 }
 
 class _ChessGameState extends State<ChessGame> {
+  List<String> squareNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -29,153 +31,63 @@ class _ChessGameState extends State<ChessGame> {
             appBar: const MyAppBar(),
             body: Container(
               color: AppTheme.backgroundColor,
-              height: double.infinity,
-              width: double.infinity,
-              child: Expanded(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width - 10,
-                              child: Hero(
-                                tag: widget.chessGameModel.gameId,
-                                child: ChessBoard(
-                                  controller: state.chessBoardController!,
-                                  boardOrientation:
-                                      widget.chessGameModel.userId ==
-                                              widget.chessGameModel.whitePlayer
-                                          ? PlayerColor.white
-                                          : PlayerColor.black,
-                                ),
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  height: 10,
-                                  width:
-                                      (MediaQuery.of(context).size.width - 10) /
-                                          8,
-                                ),
-                              ],
-                            ),
-                          ],
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Stack(
+                    children: [
+                      Hero(
+                        tag: widget.chessGameModel.gameId,
+                        child: chess.ChessBoard(
+                          controller: state.chessBoardController!,
+                          boardOrientation: widget.chessGameModel.userId ==
+                                  widget.chessGameModel.whitePlayer
+                              ? chess.PlayerColor.white
+                              : chess.PlayerColor.black,
                         ),
-                        Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(
-                                color: Colors.black,
-                              ),
-                            ),
-                            width: 10,
-                            height: MediaQuery.of(context).size.width - 10)
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                      for (var i = 0; i < 8; i++)
+                        BottomSquareAnnotation(
+                          squareName: squareNames[i],
+                          squareNumber: i,
+                        ),
+                    ],
+                  ),
+                ],
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class BottomSquareAnnotation extends StatelessWidget {
+  const BottomSquareAnnotation({
+    super.key,
+    required this.squareName,
+    required this.squareNumber,
+  });
+
+  final String squareName;
+  final int squareNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 2,
+      left: (MediaQuery.of(context).size.width - 2) / 8 -
+          ((MediaQuery.of(context).size.width - 2) / 8 / 6) +
+          squareNumber * ((MediaQuery.of(context).size.width - 2) / 8),
+      child: Text(
+        squareName,
+        style: TextStyle(
+            color: squareNumber % 2 == 0
+                ? const Color(0xFFf0d9b5)
+                : const Color(0xFFb58863)),
       ),
     );
   }
