0694d96c96da58310290f2e8206287741d720fe8
Dawid Wróbel <wrobeldawid1997@gmail.com>
2023-03-12 19:26:14 +0100

    Fnished ChessBoard looks

--- lib/src/features/home_page/presentation/pages/chess_game.dart
+++ lib/src/features/home_page/presentation/pages/chess_game.dart
@@ -72,12 +72,18 @@ class _ChessGameState extends State<ChessGame> {
                             squareName: squareNames[i],
                             squareNumber: i,
                           ),
+                      if (game.userId == game.whitePlayer)
+                        for (var i = 0; i < 8; i++)
+                          WhiteSideSquareAnnotation(squareNumber: i),
                       if (game.userId == game.blackPlayer)
                         for (var i = 0; i < 8; i++)
                           BlackBottomSquareAnnotation(
                             squareName: squareNames.reversed.toList()[i],
                             squareNumber: i,
                           ),
+                      if (game.userId == game.blackPlayer)
+                        for (var i = 0; i < 8; i++)
+                          BlackSideSquareAnnotation(squareNumber: i),
                     ],
                   ),
                 ],
@@ -145,6 +151,66 @@ class BlackBottomSquareAnnotation extends StatelessWidget {
           squareNumber * sizeOfASingleSquare,
       child: Text(
         squareName,
+        style: TextStyle(
+            color: squareNumber % 2 == 0
+                ? const Color(0xFFf0d9b5)
+                : const Color(0xFFb58863)),
+      ),
+    );
+  }
+}
+
+class WhiteSideSquareAnnotation extends StatelessWidget {
+  const WhiteSideSquareAnnotation({
+    super.key,
+    required this.squareNumber,
+  });
+
+  final int squareNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    const int outlineOffset = 5;
+    const int margin = 3;
+    final double sizeOfASingleSquare =
+        (MediaQuery.of(context).size.width - outlineOffset) / 8;
+    return Positioned(
+      right: 2,
+      top: sizeOfASingleSquare -
+          (sizeOfASingleSquare - margin) +
+          squareNumber * sizeOfASingleSquare,
+      child: Text(
+        (8 - squareNumber).toString(),
+        style: TextStyle(
+            color: squareNumber % 2 == 0
+                ? const Color(0xFFf0d9b5)
+                : const Color(0xFFb58863)),
+      ),
+    );
+  }
+}
+
+class BlackSideSquareAnnotation extends StatelessWidget {
+  const BlackSideSquareAnnotation({
+    super.key,
+    required this.squareNumber,
+  });
+
+  final int squareNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    const int outlineOffset = 5;
+    const int margin = 3;
+    final double sizeOfASingleSquare =
+        (MediaQuery.of(context).size.width - outlineOffset) / 8;
+    return Positioned(
+      right: 2,
+      top: sizeOfASingleSquare -
+          (sizeOfASingleSquare - margin) +
+          squareNumber * sizeOfASingleSquare,
+      child: Text(
+        (squareNumber + 1).toString(),
         style: TextStyle(
             color: squareNumber % 2 == 0
                 ? const Color(0xFFf0d9b5)
